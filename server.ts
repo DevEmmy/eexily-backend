ce964e5fb8876602deeab0b636c83923fe25c146
web-tranquility <eolaosebikan60@gmail.com>
2024-07-12 19:39:21 +0100

    added docs

--- server.ts
+++ server.ts
@@ -4,6 +4,9 @@ import cors from 'cors'
 import corsOptions from './src/config/cors';
 require("dotenv").config()
 import userRouter from "./src/router/UserRouter"
+import gasRouter from "./src/router/gasRoutes"
+import swaggerJSDoc from 'swagger-jsdoc';
+import swaggerUI from "swagger-ui-express"
 
 const app = express();
 const port = String(process.env.PORT) || 3030;
@@ -16,12 +19,41 @@ app.use(cors({
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb' }))
 
+const options = {
+  definition: {
+    openapi: "3.1.0",
+    info: {
+      title: "Bettisports API",
+      version: "0.1.0",
+      description:
+        "This is the docs for all APIs for Eexily",
+    },
+    servers: [
+      {
+        url: "https://eexily-backend.onrender.com",
+      },
+    ],
+  },
+  apis: ["./src/docs/*.ts"],
+};
+
+const specs = swaggerJSDoc(options);
+app.use(
+  "/api-docs",
+  swaggerUI.serve,
+  swaggerUI.setup(specs, { explorer: true,
+    customCssUrl:
+    "https://cdn.jsdelivr.net/npm/swagger-ui-themes@3.0.0/themes/3.x/theme-newspaper.css",
+  })
+);
+
 // Run MongoDB
 mongoose.connect(process.env.MONGODB_URI as string)
 const connection = mongoose.connection
 connection.once('open', () => { console.log('Database running Successfully') });
 
 app.use("/auth", userRouter)
+app.use("/gas", gasRouter)
 
 //render the html file
 app.get('/', (req, res) => {
