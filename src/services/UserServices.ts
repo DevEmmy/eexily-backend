670ec0f3feb0c0ded9a2a6eda7e57fa4d7e8935a
web-tranquility <eolaosebikan60@gmail.com>
2024-07-12 14:29:11 +0100

    updated sign up flow to create gas objec

--- src/services/UserServices.ts
+++ src/services/UserServices.ts
@@ -1,4 +1,4 @@
-import { UserLoginDto, userDto } from "../dto/user-dto";
+import { UserLoginDto, createUserDto, userDto } from "../dto/user-dto";
 import UserRepository from "../repositories/UserRepository";
 import jwt from "jsonwebtoken"
 require("dotenv").config();
@@ -6,32 +6,40 @@ import bcrypt from "bcrypt"
 import "reflect-metadata";
 import { Service } from "typedi";
 import mongoose from "mongoose";
+import GasRepository from "../repositories/GasRepository";
+import { GasDto } from "../dto/gas-dto";
 
 let jwtSecret = process.env.JWT_SECRET as string;
 
 @Service()
 export class UserServices {
-    constructor(private readonly repo: UserRepository) { };
+    constructor(private readonly repo: UserRepository, private readonly gasRepo: GasRepository) { };
 
     generateToken(id: string) {
         let token = jwt.sign({ id }, jwtSecret)
         return token;
     }
 
-    async signUp(data: userDto) {
+    async signUp(data: createUserDto) {
         try {
             let { email, password } = data;
+            let { size, houseHoldSize, primaryCookingAppliance } = data;
 
             let checkUser = await this.repo.findByEmail(email);
-            if(checkUser){
-                return {message: "User with this email already exists."}
+            if (checkUser) {
+                return { message: "User with this email already exists." }
             }
 
             data.password = await bcrypt.hash(password, 8);
+
             let user = await this.repo.create(data);
+
+            let gasObject: GasDto = { size, houseHoldSize, primaryCookingAppliance, ownedBy: String(user._id) };
+
+            let gas = await this.gasRepo.create(gasObject)
             let token = this.generateToken(String(user._id))
             return {
-                payload: {user, token}
+                payload: { user, token, gas }
             }
         }
         catch (err: any) {
@@ -43,18 +51,18 @@ export class UserServices {
         try {
             let { email, password } = data;
             let user = await this.repo.findByEmail(email)
-            
-            if(!user){
-                return {message: "User with this email does not exist"}
+
+            if (!user) {
+                return { message: "User with this email does not exist" }
             }
 
-            let doMatch = await bcrypt.compare(password, user.password, );
-            if(!doMatch){
-                return {message: "Incorrect Password"}
+            let doMatch = await bcrypt.compare(password, user.password,);
+            if (!doMatch) {
+                return { message: "Incorrect Password" }
             }
             let token = this.generateToken(String(user._id))
             return {
-                payload: {user, token}
+                payload: { user, token }
             }
         }
 
@@ -67,6 +75,6 @@ export class UserServices {
 
     }
 
-  
+
 
 }
